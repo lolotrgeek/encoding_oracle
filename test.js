2e9f60cac4a0abb38fad14c072e47283a3e26ac8
lolotrgeek <jon@simplyenable.com>
2023-02-18 10:09:24 -0600

    test: simple textual data

--- test.js
+++ test.js
@@ -11,3 +11,12 @@ const inputJson = [
 const flattenedAndEncoded = EncodeJson(inputJson)
 console.log(flattenedAndEncoded)
 console.log(flattenedAndEncoded.toString())
+
+const textual = [
+    { stock: 933, sold: 352, price: 0.95, category: 'drinks', id: 40 },
+    { stock: 154, sold: 103, price: 5.20, category: 'foods', id: 67 },
+    { stock: 23, sold: 5, price: 121.30, category: 'electronics', id: 150 },
+]
+
+const encodedTextual = EncodeJson(textual)
+console.log(encodedTextual.toString())
