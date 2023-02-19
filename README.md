9a07a4e8e74a9161dc35dab8d71c06f0030b4b0c
lolotrgeek <jon@simplyenable.com>
2023-02-19 08:17:03 -0600

    test: EncodeJson

--- README.md
+++ README.md
@@ -3,7 +3,5 @@ Auto encodes any JSON data into tensors.
 
 ## TODO
 needs tests
-- large json
-- deep nested
 - long strings
 - undefined, null, etc
---
+++ tests/main.test.js
@@ -0,0 +1,56 @@
+const { EncodeJson } = require('../main.js')
+const nested = require("./nested.json")
+
+
+describe('EncodeJson', () => {
+  it('returns a tensor for valid JSON objects', () => {
+    const data = {
+      a: 1,
+      b: 2,
+      c: {
+        d: 3,
+        e: [4, 5, 6],
+      },
+    }
+    const expected = expect.any(Object) // Tensor objects don't compare directly
+    const result = EncodeJson(data)
+    expect(result).toEqual(expected)
+  })
+
+  it('returns an error for invalid input', () => {
+    const data = 'not an object or array'
+    const expected = new Error('Invalid input')
+    const result = EncodeJson(data)
+    expect(result).toEqual(expected)
+  })
+
+  it('normalizes and encodes arrays', () => {
+    const data = [      { a: 1, b: { c: 2, d: 3 } },      { a: 4, b: { c: 5, d: 6 } },    ]
+    const expected = expect.any(Object) // Tensor objects don't compare directly
+    const result = EncodeJson(data)
+    expect(result).toEqual(expected)
+  })
+
+  it('handles ddeply nested objects', () => {
+    const data = nested
+    const expected = expect.any(Object) // Tensor objects don't compare directly
+    const result = EncodeJson(data)
+    expect(result).toEqual(expected)
+  })
+
+  it("handles undefined, null, and NaN", () => {
+    const data = {
+      a: undefined,
+      b: null,
+      c: NaN,
+      d: {
+        e: undefined,
+        f: null,
+        g: NaN,
+      },
+    }
+    const expected = expect.any(Object) // Tensor objects don't compare directly
+    const result = EncodeJson(data)
+    expect(result).toEqual(expected)
+  })
+})
